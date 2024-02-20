8122d52826528c8b1b8dda286f524b83e5ff02a9
dwivedialind <84173548+dwivedialind@users.noreply.github.com>
2024-02-20 18:16:07 +0530

    much better looping

--- Language/Program Structure/looping.js
+++ Language/Program Structure/looping.js
@@ -1,7 +1,11 @@
-for(let i = 0 ; i < 7 ; i++){
+/*for(let i = 0 ; i < 7 ; i++){
     let str = "";
     for(let j = 0; j <= i ; j++){
        str += "#";
     }
     console.log(str);
-}
+}*/
+
+for(let line = "#"; line.length < 8; line+="#")
+	console.log(line);
+
