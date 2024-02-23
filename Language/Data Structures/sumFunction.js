139ebc317cd9c07146b81cebf9e6749923d68184
dwivedialind <84173548+dwivedialind@users.noreply.github.com>
2024-02-23 19:44:21 +0530

    Updated sumFunction to have step

--- Language/Data Structures/sumFunction.js
+++ Language/Data Structures/sumFunction.js
@@ -1,10 +1,15 @@
-function range(start, end){
+
+function range(start, end, step=1){
     let arr = [];
-    while(start!=end+1){
+    while(start!=end){
         arr.push(start);
-        start++;
+        start +=step;
     }
+    arr.push(start);
+    
     return arr;
+
+   
 }
 
 let sum = (arr) =>{
@@ -15,4 +20,9 @@ let sum = (arr) =>{
     return total;
 }
 
-console.log(sum(range(1,10)));
+console.log(range(1, 10));
+// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(range(5, 2, -1));
+// → [5, 4, 3, 2]
+console.log(sum(range(1, 10)));
+// → 55
