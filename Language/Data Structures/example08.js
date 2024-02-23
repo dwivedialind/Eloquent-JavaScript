fbc1f13d2964d81120ca767eec53702eacc20c2c
dwivedialind <84173548+dwivedialind@users.noreply.github.com>
2024-02-23 14:24:06 +0530

    String and its properties

---
+++ Language/Data Structures/example08.js
@@ -0,0 +1,17 @@
+console.log("month".toUpperCase());
+
+let word = "month";
+console.log(word.slice());
+
+console.log("one two three".indexOf("ne"));
+
+console.log("   okay getty\n   ".trim());
+
+console.log("alim".padStart(5, "0"));
+
+let sentence= "Secretarybirds specialize in stomping";
+let words = sentence.split(" ");
+console.log(words);
+console.log(words.join("__"));
+
+console.log("RAH".repeat(2));
