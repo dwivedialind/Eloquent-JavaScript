1b0090536f1a0e7ec51dc7245bd3238f0760af78
dwivedialind <84173548+dwivedialind@users.noreply.github.com>
2024-02-24 13:20:53 +0530

    Higher Order functions

---
+++ Language/Higher Order Functions/example01.js
@@ -0,0 +1,38 @@
+//functions that create new functions
+function greaterThan(n) {
+    return m => m > n;
+}
+
+let greaterThan10 = greaterThan(10);
+console.log(greaterThan10(11));
+
+//function that change other functions
+function noisy(f){
+    return (...args) => {
+        console.log("calling with", args);
+        let result = f(...args);
+        console.log("called with", args, ", returned", result);
+        return result;
+    };
+}
+
+noisy(Math.min)(3,2,1);
+
+//function that provide new types of control flow
+function unless(test, then) {
+    if (!test) then();
+  }
+  
+  function repeat(n, action) {
+    for (let i = 0; i < n; i++) {
+      action(i);
+    }
+  }
+  repeat(3, n => {
+    unless(n % 2 == 1, () => {
+      console.log(n, "is even");
+    });
+  });
+
+
+  ["A", "B"].forEach(l => console.log(l));
