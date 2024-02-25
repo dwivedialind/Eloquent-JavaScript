ac696537d17ba4b3267e03908b9e1562424c8dd5
dwivedialind <84173548+dwivedialind@users.noreply.github.com>
2024-02-25 07:34:32 +0530

    Eloquent loop function

--- Language/Higher Order Functions/loop.js
+++ Language/Higher Order Functions/loop.js
@@ -1,9 +1,6 @@
 function loop(value, test, update, body){
-    while(test(value)){
-        body(value);
-        value = update(value);
-    }
-    return;
+  for(value; test(value); value = update(value))
+    body(value);
 }
 
-loop(-2, n=> n>0, n => n-1, console.log);
+loop(3, n=> n>0, n => n-1, console.log);
